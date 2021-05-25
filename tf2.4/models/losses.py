b132bb6e35be3193e00b51014a009eb9f521a0f8
anindo-saha <anindo@ieee.org>
2021-05-25 18:11:59 +0200

    Update Losses

--- tf2.4/models/losses.py
+++ tf2.4/models/losses.py
@@ -28,7 +28,7 @@ class Focal:
         y_pred        = tf.keras.backend.clip(y_pred, tf.keras.backend.epsilon(), 1-tf.keras.backend.epsilon()) 
         ce            = tf.math.multiply(tf.cast(y_true, tf.float32), -tf.math.log(y_pred))
         weight        = tf.math.multiply(tf.cast(y_true, tf.float32),  tf.math.pow(tf.math.subtract(1.0, y_pred), self.gamma))
-        fl            = tf.math.multiply(self.alpha, tf.math.multiply(weight, ce))
+        fl            = tf.math.multiply([1-self.alpha, self.alpha], tf.math.multiply(weight, ce))
         return tf.reduce_mean(fl)
 
 
