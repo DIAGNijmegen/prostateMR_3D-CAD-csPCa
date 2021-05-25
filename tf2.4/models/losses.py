98eaa3b5a33a865de2a7910e6936a1d6ad438920
anindo-saha <anindo@ieee.org>
2021-05-25 18:59:54 +0200

    Update Losses

--- tf2.4/models/losses.py
+++ tf2.4/models/losses.py
@@ -16,8 +16,9 @@ Update:         08/04/2021
 
 class Focal:
     """
-    Focal Loss for Segmentation with Severe Class Imbalance
     [1] T.Y. Lin et al. (2017), "Focal Loss for Dense Object Detection", IEEE CVPR. 
+    
+    Note: Function is Only Applicable for Binary Objectives.
     """
     def __init__(self, alpha=0.25, gamma=2.0):
         self.alpha    = alpha
@@ -28,22 +29,24 @@ class Focal:
         y_pred        = tf.keras.backend.clip(y_pred, tf.keras.backend.epsilon(), 1-tf.keras.backend.epsilon()) 
         ce            = tf.math.multiply(tf.cast(y_true, tf.float32), -tf.math.log(y_pred))
         weight        = tf.math.multiply(tf.cast(y_true, tf.float32),  tf.math.pow(tf.math.subtract(1.0, y_pred), self.gamma))
-        fl            = tf.math.multiply([1-self.alpha, self.alpha], tf.math.multiply(weight, ce))
+        fl            = tf.math.multiply(tf.constant([self.alpha, 1-self.alpha]), tf.math.multiply(weight, ce))
         return tf.reduce_mean(fl)
 
 
 class Weighted_Categorical_CE:
     """
     Weighted Adaptation of tf.keras.losses.CategoricalCrossentropy {weights: np.array([C1,C2,C3,...])}
+    
+    Note: Function is Only Applicable for Binary Objectives.
     """
     def __init__(self, class_weights):
-        self.weights  =  class_weights
+        self.weights  = class_weights
             
     def loss(self, y_true, y_pred):
-        weights       =  tf.convert_to_tensor(self.weights, dtype=tf.float32)
-        y_pred       /=  tf.keras.backend.sum(y_pred, axis=-1, keepdims=True)                        
-        y_pred        =  tf.keras.backend.clip(y_pred, tf.keras.backend.epsilon(), 1-tf.keras.backend.epsilon()) 
-        ce            =  tf.math.multiply(tf.cast(y_true, tf.float32), -tf.math.log(y_pred))
-        wce           =  tf.math.multiply(ce, weights)
-        loss          =  tf.keras.backend.sum(loss, -1)
+        weights       = tf.convert_to_tensor(self.weights, dtype=tf.float32)
+        y_pred       /= tf.keras.backend.sum(y_pred, axis=-1, keepdims=True)                        
+        y_pred        = tf.keras.backend.clip(y_pred, tf.keras.backend.epsilon(), 1-tf.keras.backend.epsilon()) 
+        ce            = tf.math.multiply(tf.cast(y_true, tf.float32), -tf.math.log(y_pred))
+        wce           = tf.math.multiply(ce, weights)
+        loss          = tf.keras.backend.sum(loss, -1)
         return loss
